0ed1833b2d637e2f6eb681b881354b71321ee35c
Adrian Diaz <info@adriandiaz.ca>
2014-05-23 02:04:45 -0700

    Structure
    basic pages deployed

--- javascripts/clmenu.js
+++ javascripts/clmenu.js
@@ -3,3 +3,9 @@ if (!document.getElementById) return;
 var ob = document.getElementById(objID).style;
 ob.display = (ob.display == 'block')?'none': 'block';
 }
+
+function toggleMenu2(objID) {
+if (!document.getElementById) return;
+var ob = document.getElementById(objID).style;
+ob.display = (ob.display == 'none')?'block': 'none';
+}
